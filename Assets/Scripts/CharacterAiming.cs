893243cc660333e2d74a90cb3f436dc95fca17ba
Jonattaz <jonatas.silva159@etec.sp.gov.br>
2023-07-04 21:22:34 -0300

    Projectile Raycast

--- Assets/Scripts/CharacterAiming.cs
+++ Assets/Scripts/CharacterAiming.cs
@@ -12,12 +12,15 @@ public class CharacterAiming : MonoBehaviour
     public Rig aimLayer;
 
     Camera mainCamera;
+    RaycastWeapon weapon;
+    public bool shooting;
     
     // Start is called before the first frame update
     void Start(){
         mainCamera = Camera.main;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        weapon = GetComponentInChildren<RaycastWeapon>();
     }
 
     void FixedUpdate(){
@@ -26,11 +29,33 @@ public class CharacterAiming : MonoBehaviour
             , turnSpeed * Time.deltaTime);        
     }
 
-    void Update(){
-        if(Input.GetMouseButton(1)){
-            aimLayer.weight += Time.deltaTime / aimDuration;
-        }else{
-            aimLayer.weight -= Time.deltaTime / aimDuration;
+    void LateUpdate(){
+        
+        if(aimLayer){
+            if(Input.GetMouseButton(1)){
+                aimLayer.weight += Time.deltaTime / aimDuration;
+            }else{
+                aimLayer.weight -= Time.deltaTime / aimDuration;
+            }
         }
+
+        if(Input.GetButtonDown("Fire1") || shooting){
+            weapon.StartFiring();
+        }
+
+        if(weapon.isFiring){
+            weapon.UpdateFiring(Time.deltaTime);
+        }
+
+        weapon.UpdateBullets(Time.deltaTime);
+
+        if(!shooting){
+            weapon.StopFiring();
+        }
+
+        /*if(Input.GetButtonUp("Fire1")){
+            weapon.StopFiring();
+        }*/
+
     }
 }
